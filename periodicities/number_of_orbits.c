f4eb8985ff9ebf63dc10ec5f6f24ac594e33af4c
Jeroen van der Meer <meer@vivaldi.net>
2022-04-12 12:23:36 +0000

    Computation range was too large (int overflow)

--- periodicities/number_of_orbits.c
+++ periodicities/number_of_orbits.c
@@ -26,14 +26,13 @@ int stable_orbits(int N) {
 	int step; // Keeps track of how long a simulated system has been running
 	int *temp_values; // For keeping track of the values met while simulating
 	temp_values = (int *) malloc(sizeof(int) * N);
-
+	
 	for (int start_value = 0; start_value < N; start_value++) {
 		if (found_in_orbit[start_value] == false) {
 			// Initiate some values
 			x = start_value;
 			step = 1;
 			temp_values[0] = x;
-
 			for (;;) { // Keep going with the simulation until we've either found a loop or a value that has already been considered
 				step += 1;
 				x = f(x) % N;
@@ -42,7 +41,6 @@ int stable_orbits(int N) {
 					update_values(found_in_orbit, temp_values, step);
 					goto next_start_value; 
 				}
-
 				for (int j = 0; j < step; j++) { // Check if we've landed in a loop
 					if (temp_values[j] == x) { 
 						count += 1; // If we've encountered a loop then it must a new one, so increase the count by 1
@@ -51,7 +49,6 @@ int stable_orbits(int N) {
 						goto next_start_value;
 					}
 				}
-
 				temp_values[step] = x; // If we keep going then we must add x to the temporary list
 			}
 		}
@@ -62,9 +59,8 @@ next_start_value:
 
 int main() {    
 	FILE *output;
-
 	output = fopen("output.csv", "w+");
-	for (int n = 1; n < 100000; n++) {
+	for (int n = 1; n <+ 10000; n++) {
 		int term = stable_orbits(n);
 		fprintf(output, "%d,", term);
 	}
